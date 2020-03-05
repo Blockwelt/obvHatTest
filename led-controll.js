bc0f3f201d776d5f1cb35a1f9e50fa59d2add87c
Patrik Nord <patrik.nord@sr.se>
2020-03-05 12:13:57 +0100

    bat function

--- led-controll.js
+++ led-controll.js
@@ -1,7 +1,12 @@
-const Gpio = require('onoff');
+// const Gpio = require('onoff');
 
-const pin_full = new Gpio(4, 'in');
-const pin_tq = new Gpio()
+// const pin_full = new Gpio(4, 'in');
+// const pin_tq = new Gpio(17, 'in');
+// const pin_half = new Gpio(27, 'in');
+// const pin_low = new Gpio(22, 'in');
+
+   const comparator = [pin_full, pin_tq, pin_half, pin_low]
+//const comparator2 = [true,false,false,true]
 
 const pin_red = new Gpio(6, 'out');
 const pin_green = new Gpio(16, 'out');
@@ -13,3 +18,18 @@ exports.toggle(r,g,b) =>{
     pin_green.writeSync(g ? 1:0);
     pin_blue.writeSync(b ? 1:0);
 }
+
+
+exports.bat(){
+
+    let reading = 0;
+    comparator2.forEach(pin => {
+        reading += !pin.readSync() ? 1 : 0
+        //reading += !pin ? 1 : 0
+    });
+
+
+    return reading/4
+}
+
+//console.log(bat());
